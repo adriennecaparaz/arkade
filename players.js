b1b2f397793f2bfde2049f9a6dfcdaf6950f271b
Adrienne Caparaz <adcapz@gmail.com>
2023-02-27 23:06:41 -0800

    added select a player json

---
+++ players.js
@@ -0,0 +1,9 @@
+$.getJSON("https://sheets.googleapis.com/v4/spreadsheets/1oOnMO6otmoFJgK6KGX9XFrB9skeMwFFoIkn5bVzSVrw/values/player+data?alt=json&key=AIzaSyBe5ljVM5E_KYygkK1erX0cBX8ARu0GuJ4", function (data) {
+    for (i = 1; i < data['values'].length; i++) {
+        let name = data['values'][i][0];
+        let url = data['values'][i][1];
+        let icon = data['values'][i][4];
+
+        document.getElementById('player-container').innerHTML += ('<div class="player"><div class="player-img"><a href="./players/' + url + '.html" title="' + name + '"><img src="' + icon + '"></a></div><div class="player-name"><a href="./players/' + url + '.html" title="' + name + '">' + name + '</a></div>');
+    }
+});
